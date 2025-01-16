84e7ced78b677399deb81fdfcb4342720a242df5
PavelBomberov <pavelbomberov@gmail.com>
2025-01-16 18:31:56 +0300

    feat: add board lib

--- scripts/game/game.js
+++ scripts/game/game.js
@@ -1,38 +1,10 @@
-const board = document.getElementById('board');
+const boardDiv = document.getElementById('board');
+const board = Chessboard(boardDiv, 'start')
 const squares = Array.from(document.getElementsByClassName('square'));
 
-let active;
+const removeHighlight = () => squares.forEach(elem => elem.classList.remove('highlight'));
 
-const dragStart = e => {
-    console.log(e);
-    // target.removeEventListener('dragstart', dragStart)
-    active.classList.add("dragging");
-}
+const isEmpty = square => square.children.length === 0;
 
-board.addEventListener('mousedown', e => {
-    // console.log(e)
-    const target = e.target.closest('div.draggble');
-    if (target) {
+const isSameColour = (element1, element2) => pieceColour(element1) === pieceColour(element2);
 
-        const parent = target.parentElement;
-        active = target;
-        parent.innerHtml = '';
-        active.addEventListener('dragstart', dragStart);
-    }
-})
-
-squares.forEach(element => {
-    element.addEventListener('dragover', e => {
-        e.preventDefault();
-        console.log('here')
-    })
-
-    element.addEventListener('drop', (event) => {
-        event.preventDefault();
-        console.log(event)
-        const dropzone = event.target;
-        active.removeEventListener('dragstart', dragStart)
-        dropzone.appendChild(active)
-        active.classList.remove('dragging');
-    })
-});
